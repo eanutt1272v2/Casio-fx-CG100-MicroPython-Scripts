198514b6a5e336d9c4d484fc9e3852b1571ba255
eanutt1272.v2 <eanutt1272.v2@gmail.com>
2026-07-23 17:04:08 +0100

    Update /physics directory

--- physics/psi_fx.py
+++ physics/psi_fx.py
@@ -1,254 +1,127 @@
 from casioplot import clear_screen, draw_string, set_pixel, show_screen
-from math import pi, sqrt, sin, cos
+from math import pi
 
 from psi_fx_lib import (
-    cmap_registry,
-    cmap,
-    fmt_density,
-    read_float,
+    SCR_H,
+    PY,
+    SAMP,
+    LEG_X,
+    LEG_W,
+    LEG_H,
+    LEG_LABEL_X,
+    EPSILON,
+    AMU_TO_KG,
+    BASIS_MAP,
+    PLANE_MAP,
+    UNIT_LABELS,
+    CMAP_REGISTRY,
     read_int,
+    read_float,
+    read_text,
     wait_for_exit,
     HydrogenicWavefunction,
+    get_unit_map,
+    get_R,
+    get_peak_density,
+    generate_colour_lut,
+    fmt_density,
+    format_hdr,
 )
-
-try:
-    from casioplot import getkey
-except ImportError:
-    getkey = None
-
 
 n = read_int("n (default=4): ", min_value=1, default=4)
 l = read_int("l (0..n-1, default=1): ", min_value=0, max_value=n - 1, default=1)
 m = read_int("m (-l..l, default=0): ", min_value=-l, max_value=l, default=0)
+Z = read_float("Z (default=1.0): ", min_value=0.1, default=1.0)
+
+use_mu = read_text("Reduced mass? (y/n, default=y): ", default="y").lower() == "y"
+M_kg = (read_float("Nucleus Mass [amu] (default=%s): " % (2.0 * Z), default=2.0 * Z) * AMU_TO_KG) if (use_mu and Z > 1.0) else None
 
 print("Spherical Harmonic Basis:")
-print("1: real")
-print("2: cmplx")
-basis_choice = read_int(
-    "Basis (1-2, default=1): ", min_value=1, max_value=2, default=1
-)
+for k in range(1, len(BASIS_MAP) + 1):
+    print("%s: %s" % (k, BASIS_MAP[k]))
+basis_choice = read_int("Basis (1-2, default=2): ", min_value=1, max_value=2, default=2)
+basis_str = BASIS_MAP[basis_choice]
 
 print("Slice Plane:")
-print("1: xz")
-print("2: xy")
-print("3: yz")
-plane_choice = read_int(
-    "Plane (1-3, default=1): ", min_value=1, max_value=3, default=1
-)
+for k in range(1, len(PLANE_MAP) + 1):
+    print("%s: %s" % (k, PLANE_MAP[k]))
+plane_choice = read_int("Plane (1-3, default=1): ", min_value=1, max_value=3, default=1)
+plane_str = PLANE_MAP[plane_choice]
 
 offset = read_float("offset [a0] (default=0.0): ", default=0.0)
 phi_deg = read_float("phi_deg (default=0.0): ", default=0.0)
 phi_slice = phi_deg * pi / 180.0
 
-Z = read_float("Z (default=1.0): ", min_value=0.1, default=1.0)
 R = read_float("R [a0] (0=auto, default=0.0): ", min_value=0.0, default=0.0)
-k_scale = read_float(
-    "k_scale (default=1.5): ", min_value=1.0, max_value=3.0, default=1.5
-)
-
-gamma = read_float(
-    "gamma (default=0.50): ", min_value=0.01, max_value=2.0, default=0.50
-)
+k_scale = read_float("k_scale (default=1.5): ", min_value=1.0, max_value=3.0, default=1.5)
+gamma = read_float("gamma (default=0.5): ", min_value=0.01, max_value=2.0, default=0.5)
 
 print("Prob. Density Units:")
-print("1: [a0^-3] (Atomic)")
-print("2: [m^-3] (SI Metric)")
-unit_choice = read_int(
-    "Units (1-2, default=1): ", min_value=1, max_value=2, default=1
-)
+for k in range(1, len(UNIT_LABELS) + 1):
+    print("%s: %s" % (k, UNIT_LABELS[k]))
+unit_choice = read_int("Units (1-2, default=1): ", min_value=1, max_value=2, default=1)
 
 print("Colour Maps:")
-for idx in range(len(cmap_registry)):
-    print(str(idx + 1) + ": " + cmap_registry[idx][0])
-cm_choice = read_int(
-    "Map (1-8, default=6): ", min_value=1, max_value=8, default=6
-)
-
-
-BASIS_MAP = {1: "real", 2: "cmplx"}
-basis_str = BASIS_MAP[basis_choice]
-
-PLANE_MAP = {1: "xz", 2: "xy", 3: "yz"}
-plane_str = PLANE_MAP[plane_choice]
-
-A0_M = 5.29177210903e-11
-A0_3 = A0_M * A0_M * A0_M
-UNIT_MAP = {
-    1: (1.0, " [a0^-3]"),
-    2: (A0_3, " [m^-3]")
-}
-unit_scale, unit_str = UNIT_MAP[unit_choice]
-
-cm_idx = cm_choice - 1
-cm_str, RC, GC, BC = cmap_registry[cm_idx]
+for idx in range(1, len(CMAP_REGISTRY) + 1):
+    if idx in CMAP_REGISTRY:
+        entry = CMAP_REGISTRY[idx]
+        print("%s: %s" % (idx, entry[0]))
+cm_choice = read_int("Map (1-8, default=6): ", min_value=1, max_value=8, default=6)
+cm_str = CMAP_REGISTRY[cm_choice][0]
 
 if R <= 0.0:
-    inner_term = n * n - l * (l + 1)
-    if inner_term < 0:
-        inner_term = 0
-    r_turn = (n * n + n * sqrt(inner_term)) / Z
-    R = r_turn * k_scale
-    abs_offset = abs(offset)
-    if abs_offset > 0.0:
-        R = sqrt(R * R + abs_offset * abs_offset)
+    R = get_R(n, l, Z, offset, k_scale)
 
-SCR_H = 190
-PY = 10
-SZ = SCR_H - PY
-SAMP = SZ
-LEG_X = SZ + 4
-LEG_W = 10
-LEG_LABEL_X = LEG_X + LEG_W + 2
-LEG_H = SZ
-
-wf = HydrogenicWavefunction(
-    n, l, m, Z, phi_slice, plane_choice, offset, is_real=(basis_choice == 1)
-)
-step = 2.0 * R / (SAMP - 1)
-density_3d_local = wf.density_3d
-
-
-if l == 0:
-    peak = density_3d_local(0.0, 0.0, 0.0)
-else:
-    r_ref = (n * n) / Z
-    best_ang_d = -1.0
-    th_max, ph_max = 0.0, 0.0
-
-    for r_probe in (0.6 * r_ref, 1.0 * r_ref, 1.4 * r_ref):
-        for i in range(36):
-            th = (i / 35.0) * pi
-            sin_th = sin(th)
-            z_coord = r_probe * cos(th)
-            for j in range(18):
-                ph = (j / 17.0) * 2.0 * pi
-                x_coord = r_probe * sin_th * cos(ph)
-                y_coord = r_probe * sin_th * sin(ph)
-
-                d = density_3d_local(x_coord, y_coord, z_coord)
-                if d > best_ang_d:
-                    best_ang_d = d
-                    th_max, ph_max = th, ph
-
-    sin_tm = sin(th_max)
-    cos_tm = cos(th_max)
-    cos_pm = cos(ph_max)
-    sin_pm = sin(ph_max)
-
-    peak = 1e-30
-    r_limit = r_ref * 2.5
-    r_step = r_limit / 200.0
-
-    for i in range(201):
-        r = i * r_step
-        d = density_3d_local(
-            r * sin_tm * cos_pm, r * sin_tm * sin_pm, r * cos_tm
-        )
-        if d > peak:
-            peak = d
-
-if peak < 1e-30:
-    peak = 1e-30
-
+wf = HydrogenicWavefunction(n, l, m, Z, M_kg, use_mu, phi_slice, plane_choice, offset, is_real=(basis_choice == 1))
+unit_scale, unit_str = get_unit_map(wf)[unit_choice]
+step = 2.0 * R / (SCR_H - PY - 1)
+peak = get_peak_density(n, l, m, Z, wf.density_3d)
 
 def main():
     clear_screen()
-    Zs = str(int(Z)) if Z == int(Z) else str(Z)
 
-    hdr = (
-        "|"
-        + str(n)
-        + str(l)
-        + str(m)
-        + "> Z="
-        + Zs
-        + " "
-        + basis_str
-        + " "
-        + plane_str
-        + " off:"
-        + str(offset)
-        + " "
-        + cm_str
-        + " R="
-        + str(int(R) if R == int(R) else round(R, 1))
-        + " k="
-        + str(round(k_scale, 1))
-        + " g="
-        + str(round(gamma, 2))
-        + " p="
-        + str(int(phi_deg))
-    )
-
+    hdr = format_hdr(n, l, m, Z, basis_str, plane_str, offset, cm_str, R, k_scale, phi_deg)
     draw_string(0, 0, hdr, (0, 0, 160), "small")
 
-    sp = set_pixel
-    ss = show_screen
-    get_coords_local = wf.get_coords
-    density_3d_local = wf.density_3d
-
-    color_lut = []
-    for i in range(256):
-        norm = i / 255.0
-        color_lut.append(cmap(norm, RC, GC, BC))
-
-    vmax = peak
-    vmin = 0.0
+    sp, ss = set_pixel, show_screen
+    get_coords, dens = wf.get_coords, wf.density_3d
+    colour_lut, _ = generate_colour_lut(cm_choice, CMAP_REGISTRY)
+    vmax, vmin = peak, 0.0
 
     for sy in range(SAMP):
         v = R - step * sy
-        py = PY + sy
         for sx in range(SAMP):
             u = -R + step * sx
-            x3, y3, z3 = get_coords_local(u, v)
-            d = density_3d_local(x3, y3, z3)
-
-            if d > vmax:
-                ratio = 1.0
-            elif d < vmin:
-                ratio = 0.0
-            else:
-                ratio = (d - vmin) / (vmax - vmin)
-
-            val_exposed = ratio ** gamma
-            idx = int(val_exposed * 255.0)
-
-            if idx > 255:
-                idx = 255
-            elif idx < 0:
-                idx = 0
-
-            sp(sx, py, color_lut[idx])
-    
+            x3, y3, z3 = get_coords(u, v)
+            d = dens(x3, y3, z3)
+            ratio = max(0.0, min(1.0, (d - vmin) / (vmax - vmin))) if (vmax - vmin) > 0 else 0.0
+            sp(sx, PY + sy, colour_lut[int((ratio**gamma) * 255.0)])
         ss()
 
     leg_den = LEG_H - 1 if LEG_H > 1 else 1
     for py in range(LEG_H):
-        t = 1.0 - py / leg_den
-        col = color_lut[int(t * 255.0)]
+        col = colour_lut[int((1.0 - py / leg_den) * 255.0)]
         for dx in range(LEG_W):
             sp(LEG_X + dx, PY + py, col)
+        if py in (0, LEG_H - 1):
+            for dx in range(LEG_W):
+                sp(LEG_X + dx, PY + py, (0, 0, 0))
+        sp(LEG_X, PY + py, (0, 0, 0))
+        sp(LEG_X + LEG_W - 1, PY + py, (0, 0, 0))
+        ss()
 
     for i in range(5):
         t = i / 4.0
         ty = PY + int((1.0 - t) * leg_den)
-        sp(LEG_X + LEG_W, ty, (0, 0, 0))
-        sp(LEG_X + LEG_W + 1, ty, (0, 0, 0))
-        
+        for dx in range(3):
+            sp(LEG_X + LEG_W + dx, ty, (0, 0, 0))
         t_row = 1.0 - (ty - PY) / leg_den
-        if t_row <= 0.0:
-            norm_tick = 0.0
-        else:
-            norm_tick = t_row ** (1.0 / gamma)
-            
+        norm_tick = t_row ** (1.0 / gamma + EPSILON) if t_row > 0.0 else 0.0
         d_tick = (vmin + norm_tick * (vmax - vmin)) / unit_scale
+        draw_string(LEG_LABEL_X, max(PY, min(PY + LEG_H - 8, ty - 4)), "%s %s" % (fmt_density(d_tick), unit_str), (0, 0, 0), "small")
+        ss()
 
-        label = fmt_density(d_tick) + unit_str
-        ly = max(PY, min(PY + LEG_H - 8, ty - 4))
-        draw_string(LEG_LABEL_X, ly, label, (0, 0, 0), "small")
-
-    ss()
-    wait_for_exit(getkey)
+    wait_for_exit()
 
 
 main()
--- physics/psi_fx_lib.py
+++ physics/psi_fx_lib.py
@@ -1,153 +1,58 @@
-from math import sqrt, log, exp, pi, sin, cos, atan2
+from math import sqrt, log, exp, pi, sin, cos, acos, atan2
 
+try:
+    from casioplot import getkey
+except ImportError:
+    getkey = None
 
-_lanczos_coeffs = (
-    0.99999999999980993,
-    676.5203681218851,
-    -1259.1392167224028,
-    771.32342877765313,
-    -176.61502916214059,
-    12.507343278686905,
-    -0.13857109526572012,
-    9.9843695780195716e-6,
-    1.5056327351493116e-7,
+SCR_H = 190
+PY = 10
+PX = 5
+SAMP = SCR_H - PY
+
+LEG_X = SAMP + PX
+LEG_W = 15
+LEG_H = SAMP
+
+LEG_LABEL_X = LEG_X + LEG_W + PX
+
+EPSILON = 1e-15
+
+M_E = 9.1093837e-31
+M_P = 1.67262192e-27
+A0 = 5.29177210903e-11
+AMU_TO_KG = 1.6605390666e-27
+
+BASIS_MAP = {1: "real", 2: "cmplx"}
+PLANE_MAP = {1: "xz", 2: "xy", 3: "yz"}
+UNIT_LABELS = {1: "[a0^-3] (Atomic)", 2: "[m^-3] (SI Metric)"}
+
+_LANCZOS_COEFFS = (
+    0.99999999999980993, 676.5203681218851, -1259.1392167224028,
+    771.32342877765313, -176.61502916214059, 12.507343278686905,
+    -0.13857109526572012, 9.9843695780195716e-6, 1.5056327351493116e-7,
 )
 
-
-cmap_registry = [
-    (
-        "cividis",
-        [
-            25.77607,
-            -83.187239,
-            102.370492,
-            -58.977031,
-            15.42921,
-            -0.384689,
-            -0.008973,
-        ],
-        [0.688122, -2.14075, 2.600914, -1.404197, 0.385562, 0.639494, 0.136756],
-        [-28.262533, 93.974216, -121.303164, 74.863561, -22.36376, 2.982654, 0.29417],
-    ),
-    (
-        "inferno",
-        [
-            25.092619,
-            -71.287667,
-            77.157454,
-            -41.709277,
-            11.617115,
-            0.105874,
-            0.000214,
-        ],
-        [-12.222155, 32.55388, -33.415679, 17.457724, -3.947723, 0.566364, 0.001635],
-        [-23.11565, 73.588132, -82.253923, 44.645117, -16.257323, 4.117926, -0.03713],
-    ),
-    (
-        "magma",
-        [
-            18.664253,
-            -50.758572,
-            52.170684,
-            -27.666969,
-            8.345901,
-            0.250486,
-            -0.002067,
-        ],
-        [-11.490027, 29.05388, -27.944584, 14.253853, -3.596031, 0.694455, -0.000688],
-        [-5.570769, 4.269936, 12.881091, -13.646583, 0.329057, 2.495287, -0.009548],
-    ),
-    (
-        "mako",
-        [-23.67438, 57.794682, -48.335836, 19.26673, -5.833466, 1.620032, 0.032987],
-        [-2.172825, 8.555513, -12.79364, 8.153931, -1.651402, 0.848348, 0.013232],
-        [14.259791, -47.319049, 65.176477, -44.241782, 12.702365, 0.292971, 0.040283],
-    ),
-    (
-        "plasma",
-        [-3.623823, 9.974645, -11.065106, 6.094711, -2.653255, 2.142438, 0.064053],
-        [-22.914405, 71.408341, -82.644718, 42.308428, -7.461101, 0.244749, 0.024812],
-        [18.193381, -54.020563, 60.093584, -28.491792, 3.108382, 0.742966, 0.5349],
-    ),
-    (
-        "rocket",
-        [-12.453563, 44.789992, -57.268147, 30.376433, -6.401815, 1.947267, -0.003174],
-        [52.250665, -158.313952, 173.768416, -81.403784, 15.073064, -0.476821, 0.037717],
-        [-10.648435, 11.402042, 14.869938, -21.550609, 6.253872, 0.400542, 0.112123],
-    ),
-    (
-        "turbo",
-        [-54.09554, 220.424075, -334.841257, 228.660253, -66.727306, 7.00898, 0.080545],
-        [-21.578703, 67.510842, -69.296265, 25.101273, -4.927799, 3.147611, 0.069393],
-        [110.735079, -305.386975, 288.708703, -91.680678, -10.16298, 7.655918, 0.219622],
-    ),
-    (
-        "viridis",
-        [-5.432077, 4.751787, 6.203736, -4.599932, -0.327241, 0.107708, 0.274455],
-        [4.641571, -13.749439, 14.153965, -5.758238, 0.214814, 1.39647, 0.005768],
-        [26.272108, -65.320968, 56.6563, -19.291809, 0.091977, 1.386771, 0.332664],
-    ),
-]
-
-
-def read_text(prompt, default=None):
-    while True:
-        raw = input(prompt).strip()
-        if raw:
-            return raw
-        if default is not None:
-            return default
-        print("Please enter a value.")
-
-
-def read_int(prompt, default=None, min_value=None, max_value=None):
-    while True:
-        raw = input(prompt).strip()
-        if raw == "" and default is not None:
-            value = int(default)
-        else:
-            try:
-                value = int(raw)
-            except ValueError:
-                print("Invalid integer. Try again.")
-                continue
-        if min_value is not None and value < min_value:
-            print("Value must be >= " + str(min_value))
-            continue
-        if max_value is not None and value > max_value:
-            print("Value must be <= " + str(max_value))
-            continue
-        return value
-
-
-def read_float(prompt, default=None, min_value=None, max_value=None):
-    while True:
-        raw = input(prompt).strip()
-        if raw == "" and default is not None:
-            value = float(default)
-        else:
-            try:
-                value = float(raw)
-            except ValueError:
-                print("Invalid float. Try again.")
-                continue
-        if min_value is not None and value < min_value:
-            print("Value must be >= " + str(min_value))
-            continue
-        if max_value is not None and value > max_value:
-            print("Value must be <= " + str(max_value))
-            continue
-        return value
+CMAP_REGISTRY = {
+    1: ("cividis", [25.77607, -83.187239, 102.370492, -58.977031, 15.42921, -0.384689, -0.008973], [0.688122, -2.14075, 2.600914, -1.404197, 0.385562, 0.639494, 0.136756], [-28.262533, 93.974216, -121.303164, 74.863561, -22.36376, 2.982654, 0.29417]),
+    2: ("inferno", [25.092619, -71.287667, 77.157454, -41.709277, 11.617115, 0.105874, 0.000214], [-12.222155, 32.55388, -33.415679, 17.457724, -3.947723, 0.566364, 0.001635], [-23.11565, 73.588132, -82.253923, 44.645117, -16.257323, 4.117926, -0.03713]),
+    3: ("magma", [18.664253, -50.758572, 52.170684, -27.666969, 8.345901, 0.250486, -0.002067], [-11.490027, 29.05388, -27.944584, 14.253853, -3.596031, 0.694455, -0.000688], [-5.570769, 4.269936, 12.881091, -13.646583, 0.329057, 2.495287, -0.009548]),
+    4: ("mako", [-23.67438, 57.794682, -48.335836, 19.26673, -5.833466, 1.620032, 0.032987], [-2.172825, 8.555513, -12.79364, 8.153931, -1.651402, 0.848348, 0.013232], [14.259791, -47.319049, 65.176477, -44.241782, 12.702365, 0.292971, 0.040283]),
+    5: ("plasma", [-3.623823, 9.974645, -11.065106, 6.094711, -2.653255, 2.142438, 0.064053], [-22.914405, 71.408341, -82.644718, 42.308428, -7.461101, 0.244749, 0.024812], [18.193381, -54.020563, 60.093584, -28.491792, 3.108382, 0.742966, 0.5349]),
+    6: ("rocket", [-12.453563, 44.789992, -57.268147, 30.376433, -6.401815, 1.947267, -0.003174], [52.250665, -158.313952, 173.768416, -81.403784, 15.073064, -0.476821, 0.037717], [-10.648435, 11.402042, 14.869938, -21.550609, 6.253872, 0.400542, 0.112123]),
+    7: ("turbo", [-54.09554, 220.424075, -334.841257, 228.660253, -66.727306, 7.00898, 0.080545], [-21.578703, 67.510842, -69.296265, 25.101273, -4.927799, 3.147611, 0.069393], [110.735079, -305.386975, 288.708703, -91.680678, -10.16298, 7.655918, 0.219622]),
+    8: ("viridis", [-5.432077, 4.751787, 6.203736, -4.599932, -0.327241, 0.107708, 0.274455], [4.641571, -13.749439, 14.153965, -5.758238, 0.214814, 1.39647, 0.005768], [26.272108, -65.320968, 56.6563, -19.291809, 0.091977, 1.386771, 0.332664]),
+}
 
 
 def lgamma(x):
     if x < 0.5:
-        return log(pi / sin(pi * x)) - lgamma(1.0 - x)
+        return log(pi / (sin(pi * x) + EPSILON)) - lgamma(1.0 - x)
     x -= 1.0
-    base_a = _lanczos_coeffs[0]
+    base_a = _LANCZOS_COEFFS[0]
     t = x + 7.5
     for i in range(1, 9):
-        base_a += _lanczos_coeffs[i] / (x + i)
+        base_a += _LANCZOS_COEFFS[i] / (x + i)
     return 0.5 * log(2 * pi) + (x + 0.5) * log(t) - t + log(base_a)
 
 
@@ -156,38 +61,6 @@ def horner(coeffs, t):
     for i in range(1, len(coeffs)):
         v = v * t + coeffs[i]
     return v
-
-
-def cmap(t, rc, gc, bc):
-    r = max(0, min(255, int(horner(rc, t) * 255.0)))
-    g = max(0, min(255, int(horner(gc, t) * 255.0)))
-    b = max(0, min(255, int(horner(bc, t) * 255.0)))
-    return (r, g, b)
-
-
-def fmt_density(d):
-    if d <= 0.0:
-        return "0"
-    exp_n = 0
-    v = d
-    while v >= 10.0:
-        v /= 10.0
-        exp_n += 1
-    while v < 1.0:
-        v *= 10.0
-        exp_n -= 1
-    m = round(v * 100.0) / 100.0
-    if m >= 10.0:
-        m /= 10.0
-        exp_n += 1
-    return "{:.2f}".format(m) + "e" + str(exp_n)
-
-
-def wait_for_exit(getkey):
-    if getkey is not None:
-        getkey()
-    else:
-        input("\nPress any key to exit: ")
 
 
 def _al(ll, mm, x):
@@ -225,31 +98,170 @@ def _lag(p, alp, x):
     return L1
 
 
+def get_R(n, l, Z, offset, k_scale):
+    inner_term = max(0, n * n - l * (l + 1))
+    r_turn = (n * n + n * sqrt(inner_term)) / Z
+    R = r_turn * k_scale
+    if abs(offset) > 0.0:
+        R = sqrt(R * R + offset * offset)
+    return R
+
+
+def radial_density_sq(n, l, x):
+    if x < 0:
+        return 0.0
+    val = exp(-0.5 * x) * (x**l) * _lag(n - l - 1, 2 * l + 1, x)
+    return val * val
+
+
+def angular_density_sq(l, m, u):
+    val = _al(l, abs(m), max(-1.0, min(1.0, u)))
+    return val * val
+
+
+def get_peak_density(n, l, m, Z, density_3d_local):
+    steps_r = 1024
+    b_r = 2.0 * n * (n + 1.0)
+    grid_r = [b_r * i / steps_r for i in range(steps_r + 1)]
+    best_i_r = 0
+    max_rv = -1.0
+    for i in range(len(grid_r)):
+        x_val = grid_r[i]
+        v = radial_density_sq(n, l, x_val)
+        if v > max_rv:
+            max_rv = v
+            best_i_r = i
+    a = grid_r[max(0, best_i_r - 1)]
+    b = grid_r[min(steps_r, best_i_r + 1)]
+    for _ in range(64):
+        c = a + (b - a) * 0.38197
+        d = b - (b - a) * 0.38197
+        if radial_density_sq(n, l, c) < radial_density_sq(n, l, d):
+            a = c
+        else:
+            b = d
+    x_max = (a + b) / 2.0
+    steps_u = 512
+    grid_u = [-1.0 + 2.0 * i / steps_u for i in range(steps_u + 1)]
+    best_i_u = 0
+    max_uv = -1.0
+    for i in range(len(grid_u)):
+        u_val = grid_u[i]
+        v = angular_density_sq(l, m, u_val)
+        if v > max_uv:
+            max_uv = v
+            best_i_u = i
+    a = grid_u[max(0, best_i_u - 1)]
+    b = grid_u[min(steps_u, best_i_u + 1)]
+    for _ in range(64):
+        c = a + (b - a) * 0.38197
+        d = b - (b - a) * 0.38197
+        if angular_density_sq(l, m, c) < angular_density_sq(l, m, d):
+            a = c
+        else:
+            b = d
+    u_max = (a + b) / 2.0
+    r_peak = x_max * n / (2.0 * Z)
+    th_peak = acos(max(-1.0, min(1.0, u_max)))
+    peak = density_3d_local(r_peak * sin(th_peak), 0.0, r_peak * cos(th_peak))
+    return max(peak, 1e-30)
+
+
+def format_hdr(n, l, m, Z, basis, plane, offset, cm_str, R, k, phi):
+    Zs = int(Z)
+    if Z != Zs:
+        Zs = Z 
+    return "|%s,%s,%s> Z=%s offset=%.1f phi=%.1f R=%.1f k=%.1f %s %s %s" % (
+        n, l, m, Zs, offset, phi, R, k, plane, basis, cm_str
+    )
+
+
+def get_unit_map(wf):
+    return {1: (1.0, "[a0^-3]"), 2: (wf.a_mu * wf.a_mu * wf.a_mu, "[m^-3]")}
+
+
+def generate_colour_lut(cmap_idx, registry):
+    cm_str, RC, GC, BC = registry[cmap_idx]
+    return [cmap(i / 255.0, RC, GC, BC) for i in range(256)], cm_str
+
+
+def cmap(t, rc, gc, bc):
+    return (
+        max(0, min(255, int(horner(rc, t) * 255.0))),
+        max(0, min(255, int(horner(gc, t) * 255.0))),
+        max(0, min(255, int(horner(bc, t) * 255.0))),
+    )
+
+
+def fmt_density(d):
+    return "0" if d <= 0 else "%.2e" % d
+
+def read_text(prompt, default=None):
+    while True:
+        raw = input(prompt).strip()
+        if raw:
+            return raw
+        if default is not None:
+            return default
+        print("Please enter a value.")
+
+
+def read_int(prompt, default=None, min_value=None, max_value=None):
+    while True:
+        raw = input(prompt).strip()
+        if raw == "" and default is not None:
+            value = int(default)
+        else:
+            try:
+                value = int(raw)
+            except ValueError:
+                print("Invalid integer. Try again.")
+                continue
+        if (min_value is not None and value < min_value) or (max_value is not None and value > max_value):
+            print("Out of range.")
+            continue
+        return value
+
+
+def read_float(prompt, default=None, min_value=None, max_value=None):
+    while True:
+        raw = input(prompt).strip()
+        if raw == "" and default is not None:
+            value = float(default)
+        else:
+            try:
+                value = float(raw)
+            except ValueError:
+                print("Invalid float. Try again.")
+                continue
+        if (min_value is not None and value < min_value) or (max_value is not None and value > max_value):
+            print("Out of range.")
+            continue
+        return value
+
+
+def wait_for_exit():
+    if getkey is None:
+        input("\nPress any key to exit: ")
+        return
+        
+    pass
+
+
 class HydrogenicWavefunction:
-    def __init__(self, n, l, m, Z, phi_slice, plane_choice, offset, is_real=True):
+    def __init__(self, n, l, m, Z, M_kg, use_reduced_mass, phi_slice, plane_choice, offset, is_real=False):
         self.n, self.l, self.m, self.abs_m = n, l, m, abs(m)
-        self.plane_choice, self.offset, self.phi_slice = (
-            plane_choice,
-            offset,
-            phi_slice,
-        )
+        self.plane_choice, self.offset, self.phi_slice = plane_choice, offset, phi_slice
         self.is_real = is_real
-        self.p_rad, self.alpha_l, self.rho_k = n - l - 1, 2 * l + 1, 2.0 * Z / n
-        self.log_norm_r = 0.5 * (
-            3 * log(self.rho_k)
-            + lgamma(n - l)
-            - log(2.0 * n)
-            - lgamma(n + l + 1)
-        )
-        self.norm_r = exp(self.log_norm_r)
-        self.log_norm_y = 0.5 * (
-            log((2 * l + 1) / (4 * pi))
-            + lgamma(l - self.abs_m + 1)
-            - lgamma(l + self.abs_m + 1)
-        )
+        mu = (M_E * M_P) / (M_E + M_P) if (use_reduced_mass and M_kg is None) else (M_E * M_kg / (M_E + M_kg) if use_reduced_mass else M_E)
+        self.a_mu = A0 * (M_E / mu)
+        self.p_rad, self.alpha_l = n - l - 1, 2 * l + 1
+        self.rho_k_grid = 2.0 * Z / n
+        self.norm_r = exp(1.5 * log(self.rho_k_grid) + 0.5 * (lgamma(n - l) - log(2.0 * n) - lgamma(n + l + 1)))
+        log_norm_y = 0.5 * (log((2 * l + 1) / (4 * pi)) + lgamma(l - self.abs_m + 1) - lgamma(l + self.abs_m + 1))
         if self.is_real and m != 0:
-            self.log_norm_y += 0.5 * log(2.0)
-        self.y_norm = exp(self.log_norm_y)
+            log_norm_y += 0.5 * log(2.0)
+        self.y_norm = exp(log_norm_y)
 
     def get_coords(self, u, v):
         if self.plane_choice == 1:
@@ -258,33 +270,22 @@ class HydrogenicWavefunction:
             return u, v, self.offset
         return self.offset, u, v
 
-    def density_3d(self, x_3d, y_3d, z_3d):
-        r2 = x_3d * x_3d + y_3d * y_3d + z_3d * z_3d
+    def density_3d(self, x, y, z):
+        r2 = x * x + y * y + z * z
         if r2 <= 1e-24:
-            if self.l != 0 or self.abs_m > 0:
-                return 0.0
-            d = (
-                self.norm_r
-                * _lag(self.p_rad, self.alpha_l, 0.0)
-                * self.y_norm
-                * _al(self.l, self.abs_m, 1.0)
+            return (
+                (self.norm_r * _lag(self.p_rad, self.alpha_l, 0.0) * self.y_norm * _al(self.l, self.abs_m, 1.0)) ** 2
+                if self.l == 0 and self.abs_m == 0
+                else 0.0
             )
-            return d * d
-        r = sqrt(r2)
-        rho = self.rho_k * r
+        r_grid = sqrt(r2)
+        rho = self.rho_k_grid * r_grid
         ea = -0.5 * rho + (self.l * log(rho) if self.l > 0 else 0.0)
         if ea < -700.0:
             return 0.0
         rv = self.norm_r * exp(ea) * _lag(self.p_rad, self.alpha_l, rho)
-        ct = max(-1.0, min(1.0, z_3d / r))
-        yv = self.y_norm * _al(self.l, self.abs_m, ct)
+        yv = self.y_norm * _al(self.l, self.abs_m, max(-1.0, min(1.0, z / (r_grid + EPSILON))))
         if self.is_real:
-            if self.m == 0:
-                phase = 1.0
-            else:
-                p = atan2(y_3d, x_3d)
-                phase = cos(self.m * p) if self.m > 0 else sin(self.abs_m * p)
-            val = rv * yv * phase
-            return val * val
-        return rv * yv * rv * yv
-        
+            phase = 1.0 if self.m == 0 else (cos(self.m * atan2(y, x)) if self.m > 0 else sin(self.abs_m * atan2(y, x)))
+            return rv * yv * phase * rv * yv * phase
+        return rv * rv * yv * yv
